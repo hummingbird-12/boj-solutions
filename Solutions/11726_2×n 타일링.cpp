c5a58c4ddbb564aeddf8938bbff6bb0135adbc88
hummingbird-12 <goodboyih96@gmail.com>
2019-04-26 11:53:48 -0700

    update problem 11726

--- Solutions/11726_2×n 타일링.cpp
+++ Solutions/11726_2×n 타일링.cpp
@@ -26,23 +26,20 @@ using namespace std;
 #define MOD 10007
 
 /*
- * dp[i][0] : fill in i-th column with 2x1 piece
- * dp[i][1] : fill in i-th column with 1x2 pieces
+ * dp[i] : number of combination until i-th column
  */
-int dp[1001][2];
+int dp[1002];
 
 int main() {
     int n, calc;
 
     scanf("%d", &n);
 
-    dp[1][0] = dp[1][1] = 1;
-    for (int i = 2; i <= n; i++) {
-        calc = (dp[i - 1][0] + dp[i - 2][1]) % MOD;
-        dp[i][0] = calc;
-        dp[i][1] = calc;
+    dp[1] = 1;
+    for (int i = 2; i <= n + 1; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
     }
 
-    printf("%d\n", (dp[n][0] + dp[n - 1][1]) % MOD);
+    printf("%d\n", dp[n + 1]);
     return 0;
 }
