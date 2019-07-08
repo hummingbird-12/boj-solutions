f5c7407f8c64c7ca3b89454f1bda8bddbc20921e
hummingbird-12 <goodboyih96@gmail.com>
2019-07-08 14:36:36 -0700

    problem 9095

--- Solutions/9095_1, 2, 3 더하기.cpp
+++ Solutions/9095_1, 2, 3 더하기.cpp
@@ -23,7 +23,36 @@
 
 using namespace std;
 
+#define MAX 12
+
+short mem[MAX];
+
+void backtracking(int num, short sum) {
+    if (sum >= num) {
+        if (sum == num) {
+            mem[num]++;
+        }
+        return;
+    }
+
+    for (short i = 1; i <= 3; i++) {
+        backtracking(num, sum + i);
+    }
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
+    int t, n;
+
+    scanf("%d", &t);
+
+    while (t--) {
+        scanf("%d", &n);
+        if (mem[n] == 0) {
+            backtracking(n, 0);
+        }
+        printf("%d\n", mem[n]);
+    }
+    
     return 0;
 }
