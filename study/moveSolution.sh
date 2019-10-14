7f6a428603a533bfcb7a5606e5841f902f869549
hummingbird-12 <goodboyih96@gmail.com>
2019-10-13 23:57:34 -0700

    Add script to move Source.cpp to Solutions directory

---
+++ study/moveSolution.sh
@@ -0,0 +1,37 @@
+if [[ -z "$1" ]] || [[ -z "$2" ]]; then
+    echo "Please provide the problem's number and name: ./moveSolution.sh 123 \"MY PROBLEM\""
+    exit
+fi
+
+SOURCE_FILE_NAME="Source.cpp"
+
+if [[ ! -e ./$SOURCE_FILE_NAME ]]; then
+    echo "$SOURCE_FILE_NAME file is not present in current directory."
+    exit
+fi
+
+FILE_NAME="$1_$2.cpp"
+DESTINATION_PATH="../Solutions/$FILE_NAME"
+
+if [[ -e $DESTINATION_PATH ]]; then
+    while true; do
+        echo "$FILE_NAME already exists in ../Solutions directory. Do you want to continue (Y/n)?"
+        read check
+        if [[ $check == "n" ]] || [[ $check == "Y" ]]; then
+            break
+        fi
+    done
+    if [[ $check == "n" ]]; then
+        echo "Process cancelled."
+        exit
+    else
+        echo "Overwriting file..."
+    fi
+fi
+
+
+cp ./$SOURCE_FILE_NAME $DESTINATION_PATH.CRLF
+tr -d '\r' < $DESTINATION_PATH.CRLF > $DESTINATION_PATH
+rm $DESTINATION_PATH.CRLF
+
+echo "$DESTINATION_PATH has been created with LF line ending."
