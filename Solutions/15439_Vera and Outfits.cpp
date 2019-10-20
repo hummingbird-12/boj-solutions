acb6cd067bdda8db1cd150557691d59083c8fd8e
hummingbird-12 <goodboyih96@gmail.com>
2019-10-20 02:38:57 -0700

    problem 15439

---
+++ Solutions/15439_Vera and Outfits.cpp
@@ -0,0 +1,38 @@
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cassert>
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <deque>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+    cout << n * (n - 1) << '\n';
+
+    return 0;
+}
