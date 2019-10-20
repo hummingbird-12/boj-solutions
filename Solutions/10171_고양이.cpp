fbe43687f8e4ec42bb61f9bb9d1e29c70bb31fe5
hummingbird-12 <goodboyih96@gmail.com>
2019-10-19 23:46:27 -0700

    problem 10171

---
+++ Solutions/10171_고양이.cpp
@@ -0,0 +1,39 @@
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cassert>
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <deque>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    string str =
+        "\\    /\\\n"
+        " )  ( ')\n"
+        "(  /  )\n"
+        " \\(__)|\n";
+
+    cout << str;
+    return 0;
+}
