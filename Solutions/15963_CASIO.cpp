f673ed4cf498a6f5f8c65c306424f0123981e014
Inho Kim <goodboyih96@gmail.com>
2019-10-20 17:47:27 -0700

    problem 15963

---
+++ Solutions/15963_CASIO.cpp
@@ -0,0 +1,39 @@
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cassert>
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    long long n, m;
+    cin >> n >> m;
+    cout << (n == m ? 1 : 0) << '\n';
+
+    return 0;
+}
