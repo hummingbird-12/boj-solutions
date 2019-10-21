2ff18b8e746e605e0762a4188ec5f1ebdec1467c
hummingbird-12 <goodboyih96@gmail.com>
2019-10-20 17:32:08 -0700

    problem 1330

---
+++ Solutions/1330_두 수 비교하기.cpp
@@ -0,0 +1,47 @@
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cassert>
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    int a, b;
+    cin >> a >> b;
+    if (a > b) {
+        cout << ">\n";
+    }
+    else if (a == b) {
+        cout << "==\n";
+    }
+    else {
+        cout << "<\n";
+    }
+
+    return 0;
+}
