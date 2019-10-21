2c19e4f491238b99e879db8ba17c868cc65966c7
hummingbird-12 <goodboyih96@gmail.com>
2019-10-21 00:29:22 -0700

    problem 2445

---
+++ Solutions/2445_별 찍기 - 8.cpp
@@ -0,0 +1,63 @@
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cassert>
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << '*';
+        }
+        for (int j = 0; j < (n - i) * 2; j++) {
+            cout << ' ';
+        }
+        for (int j = 0; j < i; j++) {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = 0; j < i; j++) {
+            cout << '*';
+        }
+        for (int j = 0; j < (n - i) * 2; j++) {
+            cout << ' ';
+        }
+        for (int j = 0; j < i; j++) {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
