f416c4624f935b5bec542d67c77e35958589d1fc
hummingbird-12 <goodboyih96@gmail.com>
2020-01-06 23:56:24 -0800

    problem 2588

---
+++ Solutions/2588_곱셈.cpp
@@ -0,0 +1,47 @@
+#include <cassert>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    int num1, num2, product;
+    cin >> num1 >> num2;
+
+    product = num1 * num2;
+    while (num2) {
+        cout << num1 * (num2 % 10) << '\n';
+        num2 /= 10;
+    }
+    cout << product << '\n';
+
+    return 0;
+}
