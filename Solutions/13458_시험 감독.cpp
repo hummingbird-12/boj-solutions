dcaac29ffe110ad8fb3a26c549f97b23d773a96d
hummingbird-12 <goodboyih96@gmail.com>
2020-06-05 19:48:22 +0900

    problem 13458

---
+++ Solutions/13458_시험 감독.cpp
@@ -0,0 +1,55 @@
+#include <cassert>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    int rooms, per_mod, per_submod, q;
+
+    cin >> rooms;
+    vector < int > takers(rooms);
+    for (int i = 0; i < rooms; i++) {
+        cin >> takers[i];
+    }
+
+    cin >> per_mod >> per_submod;
+
+    long long count = 0;
+    for (int& t : takers) {
+        q = max(t - per_mod, 0);
+        count += 1 + q / per_submod + (q % per_submod == 0 ? 0 : 1);
+    }
+
+    cout << count << '\n';
+
+    return 0;
+}
