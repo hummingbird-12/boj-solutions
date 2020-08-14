2d21255fe76fa50eae082ead680c00da834771f4
hummingbird-12 <goodboyih96@gmail.com>
2020-08-14 17:36:18 +0900

    Update problem 5557

--- Solutions/5557_1학년.cpp
+++ Solutions/5557_1학년.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <cctype>
 #include <climits>
 #include <cmath>
@@ -5,67 +6,60 @@
 #include <cstdlib>
 #include <cstring>
 #include <ctime>
-#include <cassert>
 
 #include <algorithm>
 #include <array>
 #include <bitset>
 #include <deque>
+#include <functional>
+#include <iomanip>
 #include <iostream>
 #include <iterator>
 #include <limits>
 #include <list>
 #include <map>
+#include <numeric>
 #include <queue>
 #include <set>
 #include <sstream>
 #include <stack>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
-long long calculateSums(const vector < short >& numbers) {
-    constexpr short MAX_SUM = 20;
-    constexpr short MIN_SUM = 0;
-    /// dp[i][j]: the number of cases in which the cumulative sum till
-    /// number at index `i` is `j`.
-    vector < vector < long long > > dp;
-    dp.resize(numbers.size() - 1);
+constexpr int MIN = 0;
+constexpr int MAX = 20;
 
-    dp[0] = vector < long long >(MAX_SUM - MIN_SUM + 1, 0);
-    dp[0][numbers[0]] = 1;
-
-    for (int i = 1; i < dp.size(); i++) {
-        dp[i] = vector < long long >(MAX_SUM - MIN_SUM + 1, 0);
-        for (int j = MIN_SUM; j <= MAX_SUM; j++) {
-            if (j - numbers[i] >= MIN_SUM) {
-                dp[i][j] += dp[i - 1][j - numbers[i]];
-            }
-            if (j + numbers[i] <= MAX_SUM) {
-                dp[i][j] += dp[i - 1][j + numbers[i]];
-            }
-        }
-    }
-
-    return dp.back()[numbers.back()];
-}
+long long dp[99][21];
 
 int main() {
     cin.tie(nullptr);
     ios_base::sync_with_stdio(false);
 
-    int n, num;
-    vector < short > numbers;
-
+    int n;
     cin >> n;
-    numbers.reserve(n);
 
-    while (n--) {
-        cin >> num;
-        numbers.push_back(num);
+    int d;
+    cin >> d;
+    dp[0][d] = 1;
+    for (int i = 1; i < n - 1; i++) {
+        cin >> d;
+        for (int j = MIN; j <= MAX; j++) {
+            // Able to substract current number
+            if (j - d >= MIN) {
+                dp[i][j] += dp[i - 1][j - d];
+            }
+            // Able to add current number
+            if (j + d <= MAX) {
+                dp[i][j] += dp[i - 1][j + d];
+            }
+        }
     }
-    cout << calculateSums(numbers) << '\n';
+
+    cin >> d;
+    cout << dp[n - 2][d] << '\n';
 
     return 0;
 }
