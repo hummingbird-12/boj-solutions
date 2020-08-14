900be11961e6387e65bce5f1fa1addcc142ac288
hummingbird-12 <goodboyih96@gmail.com>
2020-08-14 14:31:01 +0900

    problem 13251

---
+++ Solutions/13251_조약돌 꺼내기.cpp
@@ -0,0 +1,61 @@
+#include <cassert>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    int m, k;
+    int n = 0;
+
+    cin >> m;
+    vector < int > cnt(m + 1);
+
+    for (int i = 1; i <= m; i++) {
+        cin >> cnt[i];
+        n += cnt[i];
+    }
+
+    long double prob = 0;
+
+    cin >> k;
+    for (int c : cnt) {
+        if (c >= k) {
+            long double mul = 1;
+            for (int i = 0; i < k; i++) {
+                mul *= ((long double)(c - i) / (n - i));
+            }
+            prob += mul;
+        }
+    }
+    printf("%.18Lf\n", prob);
+
+    return 0;
+}
