d257678978039194be4b8d536350a846a773d4a0
hummingbird-12 <goodboyih96@gmail.com>
2020-08-19 20:07:23 +0900

    problem 5719

--- Solutions/5719_거의 최단 경로.cpp
+++ Solutions/5719_거의 최단 경로.cpp
@@ -29,132 +29,101 @@
 
 using namespace std;
 
-constexpr int NO_PATH_FOUND = -1;
+vector < vector < pair < int, int > > > graph;
+vector < vector < int > > shortest_paths; // Saves parent for each node within shortest path
 
-vector < vector < bool > > removeShortestPaths(
-    const vector < vector < short > > parentsTrace,
-    const int destination
-) {
-    const int vertices = parentsTrace.size();
-    queue < short > bfsQueue;
-    vector < bool > visited(vertices, false);
-    vector < vector < bool > > removed(
-        vertices,
-        vector < bool >(vertices, false)
-    ); // removed[i][j] : whether the edge from `i` to `j` has been removed
-    int current;
+int dijkstra(const int n, const int start, const int dest) {
+    priority_queue <
+        pair < int, int >,
+        vector < pair < int, int > >,
+        greater < pair < int, int > >
+    > pq;
+    vector < int > distance(n, INT_MAX);
 
-    bfsQueue.push(destination);
-    while (!bfsQueue.empty()) {
-        current = bfsQueue.front();
-        bfsQueue.pop();
+    pq.emplace(0, start);
+    distance[start] = 0;
 
-        for (auto parent : parentsTrace[current]) {
-            removed[parent][current] = true;
-            if (!visited[parent]) {
-                visited[parent] = true;
-                bfsQueue.push(parent);
+    while (!pq.empty()) {
+        const int cur = pq.top().second;
+
+        for (const auto& edge : graph[cur]) {
+            const int& next = edge.first;
+            const int& cost = edge.second;
+
+            // Edge was removed
+            if (cost == -1) {
+                continue;
+            }
+
+            // Edge is possibly part of a shortest path
+            if (distance[cur] == distance[next] - cost) {
+                shortest_paths[next].push_back(cur);
+            }
+
+            // Update shortest distance and path
+            if (distance[cur] < distance[next] - cost) {
+                distance[next] = distance[cur] + cost;
+                pq.emplace(distance[cur] + cost, next);
+
+                shortest_paths[next].clear();
+                shortest_paths[next].push_back(cur);
             }
         }
+
+        pq.pop();
     }
 
-    return removed;
+    return (distance[dest] == INT_MAX ? -1 : distance[dest]);
 }
 
-typedef struct {
-    int shortestDistance;
-    vector < vector < short > > parentsTrace;
-} _dijkstraResult;
+/*
+ * Go backwards starting from destination, marking edges in the shortest path as removed
+ */
+void remove_shortest_paths(const int destination) {
+    queue < int > q;
 
-_dijkstraResult dijkstra(
-    const vector < vector < pair < short, int > > > graph,
-    const int start,
-    const int destination,
-    const vector < vector < bool > > removed
-) {
-    const int vertices = graph.size();
-    vector < int > distanceTo(vertices, INT_MAX);
-    priority_queue <
-        pair < int, short >,
-        vector < pair < int, short > >,
-        greater < pair < int, short > >
-    > minHeap; // pair of < DISTANCE, VERTICE >
-    vector < vector < short > > parentsTrace(vertices);
-    int currentVertice, newDistance, queuedDistance;
+    q.push(destination);
 
-    distanceTo[start] = 0;
-    minHeap.push(make_pair(0, start));
+    while (!q.empty()) {
+        const int& cur = q.front();
 
-    while (!minHeap.empty()) {
-        queuedDistance = minHeap.top().first;
-        currentVertice = minHeap.top().second;
-        minHeap.pop();
+        for (const int& par : shortest_paths[cur]) {
+            auto& edge = *find_if(graph[par].begin(), graph[par].end(),
+                [cur](const pair < int, int >& e) -> bool { return e.first == cur; });
+            edge.second = -1; // Mark edge as removed
 
-        if (queuedDistance > distanceTo[currentVertice]) {
-            continue;
+            q.push(par);
         }
-        for (auto adjacent : graph[currentVertice]) {
-            short& adjacentVertice = adjacent.first;
-            int& adjacentEdge = adjacent.second;
 
-            if (!removed.empty() && removed[currentVertice][adjacentVertice]) {
-                continue;
-            }
-            newDistance = distanceTo[currentVertice] + adjacentEdge;
-            if (newDistance >= distanceTo[adjacentVertice]) {
-                // Save all parents tracing back to shortest paths
-                if (newDistance == distanceTo[adjacentVertice]) {
-                    parentsTrace[adjacentVertice].push_back(currentVertice);
-                }
-                continue;
-            }
-            // New shortest path found
-            distanceTo[adjacentVertice] = newDistance;
-            parentsTrace[adjacentVertice] = vector < short >(1, currentVertice);
-            minHeap.push(make_pair(newDistance, adjacentVertice));
-        }
+        q.pop();
     }
-
-    _dijkstraResult result = {
-        (distanceTo[destination] == INT_MAX ? NO_PATH_FOUND : distanceTo[destination]),
-        parentsTrace
-    };
-    return result;
 }
 
 int main() {
     cin.tie(nullptr);
     ios_base::sync_with_stdio(false);
 
-    int vertices, edges, start, destination;
-    int vertice1, vertice2, distance;
-    vector < vector < pair < short, int > > > graph;
-    _dijkstraResult result;
-    vector < vector < bool > > removed;
+    int n, m, start, destination, u, v, p;
 
-    cin >> vertices >> edges;
-    while (vertices != 0 && edges != 0) {
-        graph.resize(vertices);
+    cin >> n >> m;
+    while (n != 0 || m != 0) {
+        graph.resize(n);
+        shortest_paths.resize(n);
+
         cin >> start >> destination;
-        while (edges--) {
-            cin >> vertice1 >> vertice2 >> distance;
-            // construct graph with adjacency list
-            graph[vertice1].push_back(make_pair(vertice2, distance));
+        while (m--) {
+            cin >> u >> v >> p;
+            graph[u].emplace_back(v, p);
         }
 
-        // 1. Run Dijkstra once
-        result = dijkstra(graph, start, destination, removed);
-        if (result.shortestDistance != NO_PATH_FOUND) {
-            // 2. Mark edges of shortest paths as removed
-            removed = removeShortestPaths(result.parentsTrace, destination);
-            // 3. Run Dijkstra again
-            result = dijkstra(graph, start, destination, removed);
-        }
+        dijkstra(n, start, destination);
+        remove_shortest_paths(destination);
+        cout << dijkstra(n, start, destination) << '\n';
 
-        cout << result.shortestDistance << '\n';
         graph.clear();
-        removed.clear();
-        cin >> vertices >> edges;
+        shortest_paths.clear();
+
+        cin >> n >> m;
     }
 
     return 0;
